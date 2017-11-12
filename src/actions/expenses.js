7dbf71b652ebd67ec88d8104eddbe3edfba8f7a5
Matt Frawley <docfrawley@Matts-MacBook-Pro-2.local>
2017-11-12 16:13:23 -0500

    add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,11 +1,7 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
 
-// component calls action generator
-// action generator returns function
-// component dispatches function
-// function runs redux store changes
-
+// ADD_EXPENSE
 export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
     expense
@@ -14,14 +10,14 @@ export const addExpense = (expense) => ({
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
         const {
-            description = '',
+      description = '',
             note = '',
             amount = 0,
             createdAt = 0
-        } = expenseData;
+    } = expenseData;
         const expense = { description, note, amount, createdAt };
 
-        return database.ref('expenses').push(expense).then((ref) =>{
+        return database.ref('expenses').push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
                 ...expense
@@ -30,27 +26,36 @@ export const startAddExpense = (expenseData = {}) => {
     };
 };
 
+// REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
 
-export const startRemoveExpense = ({ id }) => {
+export const startRemoveExpense = ({ id } = {}) => {
     return (dispatch) => {
         return database.ref(`expenses/${id}`).remove().then(() => {
-            const expenses = [];
             dispatch(removeExpense({ id }));
         });
     };
 };
 
+// EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
 
-//SET_EXPENSES
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+};
+
+// SET_EXPENSES
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
     expenses
